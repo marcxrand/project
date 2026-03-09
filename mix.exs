4ccc76de6596fec7447071f4f3c6c9fb8bcd988b
Marc Rand <marcxrand@gmail.com>
2026-03-08 23:44:06 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Project.MixProject do
   def project do
     [
       app: :project,
-      version: "1.0.31",
+      version: "1.0.32",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
